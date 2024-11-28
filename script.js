0d46da38045ebda7a756bb8b4a796a7ec1a5409a
Thewohlin <h23thewo@du.se>
2024-11-28 14:08:12 +0100

    skapat variabler

---
+++ script.js
@@ -0,0 +1,11 @@
+const checkbox = document.getElementById('randomDiv');
+consol.log(checkbox);
+
+const textFields = document.getElementsByClassname('textField'); 
+console.log(textFields);
+
+const button = document.getElementsById('knapp')[0];
+console.log(button);
+
+const divElement = document.getElementById('randomDiv');
+console.log(divElement);

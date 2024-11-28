521e3266b9d18826ecf300732f793e977dd9f682
case-2000 <h23kajja@du.se>
2024-11-28 15:05:50 +0100

    error cleaning

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const checkbox = document.getElementById('randomDiv');
-consol.log(checkbox);
+console.log(checkbox);
 
-const textFields = document.getElementsByClassname('textField'); 
+const textFields = document.getElementsByClassName('textField'); 
 console.log(textFields);
 
-const button = document.getElementsById('knapp')[0];
+const button = document.getElementById('knapp')[0];
 console.log(button);
 
 const divElement = document.getElementById('randomDiv');
 console.log(divElement);
+
+funkis("hello", "world")
+
+function funkis(param1, param2){
+    
+}
